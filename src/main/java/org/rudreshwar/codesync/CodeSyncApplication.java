861919af6ec28a78dee5cc4b48e0241ea2826743
Rudreshwar Baranwal <baranwalrudreshwar@gmail.com>
2026-06-29 22:42:43 +0530

    Initial project setup

---
+++ src/main/java/org/rudreshwar/codesync/CodeSyncApplication.java
@@ -0,0 +1,13 @@
+package org.rudreshwar.codesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rudreshwar/codesync/CodeSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rudreshwar.codesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
