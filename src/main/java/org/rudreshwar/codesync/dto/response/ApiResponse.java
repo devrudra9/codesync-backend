b807415a256303993d621072341a70bb59d52628
Rudreshwar Baranwal <baranwalrudreshwar@gmail.com>
2026-07-10 03:33:01 +0530

    Added API and Auth Responses

--- src/main/java/org/rudreshwar/codesync/dto/response/ApiResponse.java
+++ src/main/java/org/rudreshwar/codesync/dto/response/ApiResponse.java
@@ -1,4 +1,20 @@
 package org.rudreshwar.codesync.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ApiResponse {
+
+    private boolean success;
+    private String message;
+
+    public static ApiResponse success(String message) {
+        return new ApiResponse(true, message);
+    }
+
+    public static ApiResponse error(String message) {
+        return new ApiResponse(false, message);
+    }
 }
