541193bf94bb895a559cc4a4174f437ea161217f
Rudreshwar Baranwal <baranwalrudreshwar@gmail.com>
2026-07-15 00:40:57 +0530

    Removed redundant method

--- src/main/java/org/rudreshwar/codesync/repository/UserRepository.java
+++ src/main/java/org/rudreshwar/codesync/repository/UserRepository.java
@@ -4,7 +4,6 @@ import org.rudreshwar.codesync.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -18,5 +17,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUsername(String username);
 
-    List<User> findByUsernameContainingIgnoreCase(String query);
 }
