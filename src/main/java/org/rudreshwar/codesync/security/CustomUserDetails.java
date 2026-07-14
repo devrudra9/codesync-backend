42e2746f33db9ced22ff99ab5d13c49b0ced895c
Rudreshwar Baranwal <baranwalrudreshwar@gmail.com>
2026-07-15 00:38:50 +0530

    Created CustomUserDetails for role and authorities implementing UserDetails interface

---
+++ src/main/java/org/rudreshwar/codesync/security/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package org.rudreshwar.codesync.security;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.rudreshwar.codesync.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static CustomUserDetails from(User user) {
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+        return new CustomUserDetails(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
